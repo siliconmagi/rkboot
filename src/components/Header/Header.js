8770c57d5b78d11706dc8f2b0f1c42bec1cc5d45
siliconmagi <siliconmagi@yandex.com>
2016-09-30 23:51:07 -0700

    init

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,19 +10,27 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
-import Link from '../Link';
-import Navigation from '../Navigation';
-import logoUrl from './logo-small.png';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 function Header() {
   return (
     <div className={s.root}>
+      <Navbar inverse fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">Silicon Magi</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="/about">About</NavItem>
+            <NavItem eventKey={2} href="/contact">Contact</NavItem>
+            <NavItem eventKey={3} href="/login">Login</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <div className={s.container}>
-        <Navigation className={s.nav} />
-        <Link className={s.brand} to="/">
-          <img src={logoUrl} width="38" height="38" alt="React" />
-          <span className={s.brandTxt}>Your Company</span>
-        </Link>
         <div className={s.banner}>
           <h1 className={s.bannerTitle}>React</h1>
           <p className={s.bannerDesc}>Complex web apps made easy</p>
--- src/components/Html.js
+++ src/components/Html.js
@@ -20,6 +20,7 @@ function Html({ title, description, style, script, children }) {
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
         {style && <style id="css" dangerouslySetInnerHTML={{ __html: style }} />}
       </head>
       <body>
